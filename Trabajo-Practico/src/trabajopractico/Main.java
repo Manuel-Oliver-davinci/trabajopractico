14f0797638fd51cb12228bfc948da94ea6333bb0
Manuel Oliver Nacher <manuel.oliver@davinci.edu.ar>
2021-12-05 13:49:31 -0300

    Trabajo Practico

---
+++ Trabajo-Practico/src/trabajopractico/Main.java
@@ -0,0 +1,10 @@
+package trabajopractico;
+
+public class Main {
+    public static void main(String[] args) {
+        CuentaBancaria nuevaCuentaBancaria= new CuentaBancaria("Manuel", "44596198");
+
+        nuevaCuentaBancaria.mostrarMenu();
+    }
+
+}
